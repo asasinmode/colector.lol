f96587c0def04e8b1f8ec2cea06855f37f17b332
asasinmode <skin9470@gmail.com>
2026-09-12 05:37:21 +0200

    belveth infernal & mid quest tests

--- packages/core/test/championStats/26.18.1.test.ts
+++ packages/core/test/championStats/26.18.1.test.ts
@@ -1,15 +1,19 @@
 import type { IOverrides } from '@lolcalc/core/DamageSource.ts';
+import type { IDragonName } from '@lolcalc/data/types.js';
 import assert from 'node:assert';
 import test from 'node:test';
 import { GameAbilityId } from '@lolcalc/core/GameAbilityId.ts';
 import { ITEMS_BY_NAME } from '@lolcalc/data';
 import { AbilityType, EFFECT_OBJECT_NAME } from '@lolcalc/shared';
+import { nextTick } from 'vue';
 import fixture from '../fixtures/26.18.1.fixture.json' with { type: 'json' };
 import { overridesAppliedEffect, setupDamageSource, setupPatchFixture, typedPartialDeepStrictEqual } from '../utils.ts';
 
 test.before(() => {
 	setupPatchFixture(fixture);
 });
+
+const infernalStacks: IDragonName[] = ['Infernal', 'Infernal', 'Infernal', 'Infernal'];
 
 test.only('26.18 Belveth', async (t) => {
 	t.runOnly(true);
@@ -23,6 +27,7 @@ test.only('26.18 Belveth', async (t) => {
 			},
 		},
 		abilityLevels: { q: 5, w: 5, e: 5, r: 3 },
+		items: [ITEMS_BY_NAME.giantsBelt, ITEMS_BY_NAME.giantsBelt, ITEMS_BY_NAME.giantsBelt, ITEMS_BY_NAME.overlordsBloodmail, ITEMS_BY_NAME.riftmaker, ITEMS_BY_NAME.rabadon],
 	};
 
 	await t.test('winter caressed', async () => {
@@ -56,11 +61,10 @@ test.only('26.18 Belveth', async (t) => {
 		}, damageSource);
 	});
 
-	await t.test('base', { only: true }, async () => {
+	await t.test('base', async () => {
 		const damageSource = await setupDamageSource(fixture, 'Belveth', {
 			...sourceCommon,
 			internalData: { passiveStacks: 0, hasPassiveStack: 0 },
-			items: [ITEMS_BY_NAME.giantsBelt, ITEMS_BY_NAME.giantsBelt, ITEMS_BY_NAME.giantsBelt, ITEMS_BY_NAME.overlordsBloodmail, ITEMS_BY_NAME.riftmaker, ITEMS_BY_NAME.rabadon],
 			currentAbilityResource: 0,
 		});
 
@@ -79,17 +83,98 @@ test.only('26.18 Belveth', async (t) => {
 
 		damageSource.currentHealth.value = damageSource.maxHealth.value;
 		damageSource.currentAbilityResource.value = 1;
+
+		await nextTick();
+
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
 			attackDamage: 183,
 			abilityPower: 359,
 		}, damageSource);
-		/* game shows 5259, see help page for known discrepancies */
-		assert.strictEqual(damageSource.maxHealth.value, 5258);
+		assert.strictEqual(damageSource.maxHealth.value, 5259);
 
 		damageSource.currentHealth.value = 1115;
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
 			attackDamage: 205,
 			abilityPower: 359,
+		}, damageSource);
+		assert.strictEqual(damageSource.maxHealth.value, 5283);
+	});
+
+	await t.test('4 infernals', async () => {
+		const damageSource = await setupDamageSource(fixture, 'Belveth', {
+			...sourceCommon,
+			internalData: { passiveStacks: 0, hasPassiveStack: 0 },
+			currentAbilityResource: 0,
+			dragonStacks: infernalStacks,
+		});
+
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 178,
+			abilityPower: 365,
+		}, damageSource);
+		assert.strictEqual(damageSource.maxHealth.value, 4315);
+
+		damageSource.currentHealth.value = 720;
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 197,
+			abilityPower: 365,
+		}, damageSource);
+		assert.strictEqual(damageSource.maxHealth.value, 4315);
+
+		damageSource.currentHealth.value = damageSource.maxHealth.value;
+		damageSource.currentAbilityResource.value = 1;
+		await nextTick();
+
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 205,
+			abilityPower: 392,
+		}, damageSource);
+		assert.strictEqual(damageSource.maxHealth.value, 5259);
+
+		damageSource.currentHealth.value = 650;
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 227,
+			abilityPower: 392,
+		}, damageSource);
+		assert.strictEqual(damageSource.maxHealth.value, 5283);
+	});
+
+	await t.test('4 infernals | mid quest', { only: true }, async () => {
+		const damageSource = await setupDamageSource(fixture, 'Belveth', {
+			...sourceCommon,
+			internalData: { passiveStacks: 0, hasPassiveStack: 0 },
+			currentAbilityResource: 0,
+			dragonStacks: infernalStacks,
+			roleQuest: 'mid',
+		});
+
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 185,
+			abilityPower: 386,
+		}, damageSource);
+		assert.strictEqual(damageSource.maxHealth.value, 4315);
+
+		damageSource.currentHealth.value = 650;
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 205,
+			abilityPower: 386,
+		}, damageSource);
+		assert.strictEqual(damageSource.maxHealth.value, 4315);
+
+		damageSource.currentHealth.value = damageSource.maxHealth.value;
+		damageSource.currentAbilityResource.value = 1;
+		await nextTick();
+
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 214,
+			abilityPower: 414,
+		}, damageSource);
+		assert.strictEqual(damageSource.maxHealth.value, 5259);
+
+		damageSource.currentHealth.value = 591;
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 238,
+			abilityPower: 415,
 		}, damageSource);
 		assert.strictEqual(damageSource.maxHealth.value, 5283);
 	});
